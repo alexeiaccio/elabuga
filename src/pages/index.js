8a1ea8dea1e928524382f27312b486df92b52e3f
Alexei Accio <alexei.accio@gmail.com>
2019-01-10 13:35:28 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
 import styled from '@emotion/styled'
 
@@ -7,14 +8,18 @@ const H1 = styled.div`
   color: red;
 `
 
-function IndexPage() {
+function IndexPage({ data }) {
+  console.log(data.homepage)
+
   return (
     <Fragment>
       <H1
         css={css`
-          color: green;
+          color: black;
         `}
-      >Poop</H1>
+      >
+        {data.homepage.data.title.text}
+      </H1>
     </Fragment>
   )
 }
@@ -27,3 +32,56 @@ IndexPage.propTypes = {
 
 export default IndexPage
 
+export const PageQuery = graphql`
+  query IndexQuery {
+    homepage: prismicHomepage {
+      data {
+        title {
+          text
+        }
+        description
+        date
+        image {
+          url
+          localFile {
+            childImageSharp {
+              fluid {
+                src
+              }
+            }
+          }
+          fb {
+            url
+          }
+        }
+        text {
+          html
+        }
+        info {
+          html
+        }
+        body {
+          primary {
+            historytitle {
+              text
+            }
+            historydescription
+            historyimage {
+              url
+              localFile {
+                childImageSharp {
+                  fluid {
+                    src
+                  }
+                }
+              }
+            }
+            historytext {
+              html
+            }
+          }
+        }
+      }
+    }
+  }
+`
