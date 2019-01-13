e942fde2c44e29777444178a680a323954190fef
Alexei Accio <alexei.accio@gmail.com>
2019-01-13 12:08:29 +0300

    form

---
+++ src/components/form.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const DEFAULT_STATE = {
+  contact: '',
+  msg: '',
+  name: '',
+  left: 1200,
+}
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = DEFAULT_STATE
+  }
+
+  handleUpdate = e => {
+    const { id, value } = e.target
+
+    this.setState(({ left }) => ({
+      [id]: value,
+      left: id === 'msg' ? 1200 - value.length : left,
+    }))
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const { name, contact, msg } = this.state
+    const { tag } = this.props
+
+    fetch(
+      `https://v66qfxaz26.execute-api.us-east-1.amazonaws.com/dev/send?tag=${tag}&name=${name}&contact=${contact}&msg=${msg}`
+    )
+      .then(console.log) // eslint-disable-line
+      .catch(console.error) // eslint-disable-line
+
+    this.setState(DEFAULT_STATE)
+  }
+
+  render() {
+    const { name, contact, msg, left } = this.state
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          id="name"
+          onChange={this.handleUpdate}
+          minLength="2"
+          placeholder="Напишете ваше имя"
+          type="text"
+          value={name}
+          required
+        />
+        <input
+          id="contact"
+          onChange={this.handleUpdate}
+          placeholder="Напишите ваше емеил или телефонный номер"
+          type={contact.search(/[a-zA-Z@]/) >= 0 ? 'email' : 'tel'}
+          value={contact}
+          required
+        />
+        <textarea
+          id="msg"
+          onChange={this.handleUpdate}
+          maxLength="1280"
+          placeholder="История длиной до 1200 символов"
+          value={msg}
+          required
+        />
+        <p>Осталось: {left} знаков.</p>
+        <button type="submit">Отправить</button>
+      </form>
+    )
+  }
+}
+
+Form.propTypes = {
+  tag: PropTypes.string.isRequired,
+}
+
+export default Form
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import { css } from '@emotion/core'
@@ -6,6 +6,7 @@ import styled from '@emotion/styled'
 import propPathOr from 'crocks/helpers/propPathOr'
 import 'isomorphic-fetch'
 
+import Form from '../components/form'
 import Img from '../components/img'
 import Seo from '../components/seo'
 import RichContent from '../components/rich-content'
@@ -14,109 +15,92 @@ const H1 = styled.div`
   color: red;
 `
 
-class IndexPage extends Component {
-  componentDidMount() {
-    const name = 'Poop'
-    const contact = 'poop@po.op'
-    const msg = 'Lorem ipsum'
-    fetch(
-      `https://v66qfxaz26.execute-api.us-east-1.amazonaws.com/dev/send?name=${name}&contact=${contact}&msg=${msg}`
-    )
-      .then(console.log) // eslint-disable-line
-      .catch(console.error) // eslint-disable-line
-  }
+const IndexPage = ({ data, location }) => {
+  const pageData = propPathOr(null, ['homepage', 'data'], data)
+  const pageTitle = propPathOr(null, ['title', 'text'], pageData)
+  const pageKeywords = propPathOr(null, ['seokeywords'], pageData)
+  const pageImage = propPathOr(
+    propPathOr(null, ['image', 'fb', 'url'], pageData),
+    ['image', 'fb', 'localFile', 'childImageSharp', 'fixed', 'src'],
+    pageData
+  )
+  const pathname = propPathOr('/', ['location', 'pathname'], location)
+  const title = propPathOr(null, ['title', 'text'], pageData)
+  const description = propPathOr(null, ['description'], pageData)
+  const date = propPathOr(null, ['date'], pageData)
+  const fromNow = propPathOr(null, ['fromNow'], pageData)
+  const image = propPathOr(null, ['image'], pageData)
+  const text = propPathOr(null, ['text', 'html'], pageData)
+  const info = propPathOr(null, ['info', 'html'], pageData)
+  const body = propPathOr(null, ['body'], pageData)
 
-  render() {
-    const { data, location } = this.props
-    const pageData = propPathOr(null, ['homepage', 'data'], data)
-    const pageTitle = propPathOr(null, ['title', 'text'], pageData)
-    const pageKeywords = propPathOr(null, ['seokeywords'], pageData)
-    const pageImage = propPathOr(
-      propPathOr(null, ['image', 'fb', 'url'], pageData),
-      ['image', 'fb', 'localFile', 'childImageSharp', 'fixed', 'src'],
-      pageData
-    )
-    const pathname = propPathOr('/', ['location', 'pathname'], location)
-    const title = propPathOr(null, ['title', 'text'], pageData)
-    const description = propPathOr(null, ['description'], pageData)
-    const date = propPathOr(null, ['date'], pageData)
-    const fromNow = propPathOr(null, ['fromNow'], pageData)
-    const image = propPathOr(null, ['image'], pageData)
-    const text = propPathOr(null, ['text', 'html'], pageData)
-    const info = propPathOr(null, ['info', 'html'], pageData)
-    const body = propPathOr(null, ['body'], pageData)
+  const histories = propPathOr([], ['histories', 'edges'], data)
 
-    const histories = propPathOr([], ['histories', 'edges'], data)
-
-    return (
-      <Fragment>
-        <Seo
-          pageTitle={pageTitle}
-          pageDescription={description}
-          pageKeywords={pageKeywords}
-          pageImage={pageImage}
-          pathname={pathname}
-        />
-        <H1
-          css={css`
-            color: black;
-          `}
-        >
-          {title}
-        </H1>
-        <div>{description}</div>
-        <div>
-          Cбора историй завершиться <b>{fromNow}</b> – {date}
-        </div>
-        <Img src={image} />
-        <RichContent content={text} />
-        <div>
-          {body.map(({ id, primary }) => {
-            const historytitle = propPathOr(
-              null,
-              ['historytitle', 'text'],
-              primary
-            )
-            const historydescription = propPathOr(
-              null,
-              ['historydescription'],
-              primary
-            )
-            const historyimage = propPathOr(null, ['historyimage'], primary)
-            const historytext = propPathOr(
-              null,
-              ['historytext', 'html'],
-              primary
-            )
-
-            return (
-              <div key={id}>
-                <h2>{historytitle}</h2>
-                <div>{historydescription}</div>
-                <Img src={historyimage} />
-                <RichContent content={historytext} />
-              </div>
-            )
-          })}
-        </div>
-        <RichContent content={info} />
-        {histories.map(({ node }) => {
-          const id = propPathOr(null, ['id'], node)
-          const historyDate = propPathOr(null, ['date'], node)
-          const name = propPathOr(null, ['name'], node)
-          const history = propPathOr(null, ['history'], node)
+  return (
+    <Fragment>
+      <Seo
+        pageTitle={pageTitle}
+        pageDescription={description}
+        pageKeywords={pageKeywords}
+        pageImage={pageImage}
+        pathname={pathname}
+      />
+      <H1
+        css={css`
+          color: black;
+        `}
+      >
+        {title}
+      </H1>
+      <div>{description}</div>
+      <div>
+        Cбора историй завершиться <b>{fromNow}</b> – {date}
+      </div>
+      <Img src={image} />
+      <RichContent content={text} />
+      <div>
+        {body.map(({ id, primary }) => {
+          const historytitle = propPathOr(
+            null,
+            ['historytitle', 'text'],
+            primary
+          )
+          const historydescription = propPathOr(
+            null,
+            ['historydescription'],
+            primary
+          )
+          const historyimage = propPathOr(null, ['historyimage'], primary)
+          const historytext = propPathOr(null, ['historytext', 'html'], primary)
 
           return (
             <div key={id}>
-              <p>{historyDate}</p>
-              <p>{name}</p>
-              <p>{history}</p>
+              <h2>{historytitle}</h2>
+              <div>{historydescription}</div>
+              <Img src={historyimage} />
+              <RichContent content={historytext} />
+              <Form tag={historytitle} />
             </div>
           )
         })}
-      </Fragment>
-    )
-  }
+      </div>
+      <RichContent content={info} />
+      {histories.map(({ node }) => {
+        const id = propPathOr(null, ['id'], node)
+        const historyDate = propPathOr(null, ['date'], node)
+        const name = propPathOr(null, ['name'], node)
+        const history = propPathOr(null, ['history'], node)
+
+        return (
+          <div key={id}>
+            <p>{historyDate}</p>
+            <p>{name}</p>
+            <p>{history}</p>
+          </div>
+        )
+      })}
+    </Fragment>
+  )
 }
 
 IndexPage.propTypes = {
@@ -195,6 +179,7 @@ export const PageQuery = graphql`
       edges {
         node {
           id
+          tag
           date
           name
           history
