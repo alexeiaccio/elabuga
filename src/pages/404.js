35f6ee65a5d25b70973f8f5afc86107c3725535a
Alexei Accio <alexei.accio@gmail.com>
2019-01-16 17:54:11 +0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,39 @@
-import React, { Fragment } from 'react'
+import React from 'react'
+import { css } from '@emotion/core'
+import { Link } from 'gatsby'
+
+import Button from '../components/button'
+import Layout from '../components/layout'
+import { Heading1 } from '../components/typography'
+
+const cardStyles = css`
+  ${tw(['bg-white', 'my-q48', 'px-q24', 'py-q36', 'shadow-text'])};
+`
+
+const LinkButton = Button.withComponent(Link)
 
 const NotFoundPage = () => (
-  <Fragment>
-    <h1>404 Page Not Found</h1>
-    <p>
-      Do not worry! Incorrect URLs will lead your users here when running in
-      production mode.
-    </p>
-    <i>You might want to customize this page...</i>
-  </Fragment>
+  <Layout>
+    <div css={cardStyles}>
+      <h1 css={Heading1}>404</h1>
+      <div
+        css={css`
+          ${tw(['my-q36', 'text-lg'])};
+        `}
+      >
+        Страница не найдена
+      </div>
+      <LinkButton
+        css={css`
+          ${tw(['inline-block', 'no-underline', 'text-black'])};
+        `}
+        size="lg"
+        to="/"
+      >
+        На главную
+      </LinkButton>
+    </div>
+  </Layout>
 )
 
 export default NotFoundPage
